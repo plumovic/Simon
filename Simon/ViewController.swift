97ac5873ac3fc1902ae543411f7671f0202b1a70
plumovic <student@plumovic.local>
2017-08-31 12:13:27 -0500

    hm

--- Simon/ViewController.swift
+++ Simon/ViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    @IBOutlet weak var colorsFrame: UIView!
     @IBOutlet var colorDisplays: [UIView]!
     @IBOutlet weak var messageLabel: UILabel!
+    
+    var pattern = [Int]()
+    var timer = Timer()
+    var index = 0
+    var playerTurn = false
+    var gameOver = true
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
+        pattern.append(0)
+        pattern.append(1)
+        pattern.append(2)
+        pattern.append(3)
     }
 
-    func changeColor(number: Int)
+    func flashColor(number: Int)
     {
         UIView.transition(with: colorDisplays[number], duration: 0.5, options: .transitionCrossDissolve, animations: {
                 self.colorDisplays[number].alpha = 1.0
@@ -30,8 +40,29 @@ class ViewController: UIViewController
         }
     }
     
+    func displayPattern()
+    {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(ViewController.nextColor), userInfo: nil, repeats: true)
+    }
+    
+    @objc func nextColor()
+    {
+        
+    }
+    
     @IBAction func onStartButtonPressed(_ sender: Any)
     {
         
+    }
+    @IBAction func onColorTapped(_ sender: UITapGestureRecognizer)
+    {
+        for number in 0..<colorDisplays.count
+        {
+            if colorDisplays[number].frame.contains(sender.location(in: colorsFrame))
+            {
+                flashColor(number: number)
+
+            }
+        }
     }
 }
